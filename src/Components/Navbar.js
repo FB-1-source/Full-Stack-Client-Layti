c05a3b769e3f1397c3b87dae4a1227cd46a407ea
Felmon <felmon@Felmons-MacBook-Air.local>
2021-10-29 19:59:03 -0400

    button/search update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,13 +5,14 @@ import {Link} from "react-router-dom"
 import axios from "axios"
 import "../Styles/Start.css"
 import "../Styles/Navbar.css"
+import { useHistory} from "react-router-dom";
 
 
 function Navbar() {
 
 const [term,setterm] = useState("");
 const [schoolslist,setschools] = useState([]);
-
+let history = useHistory()
 
 const Find = (event) => {
   setterm(event.target.value)
@@ -36,7 +37,7 @@ useEffect(() => {
      return val
     }
    }).map((val) => {
-    return <div key={val.id} onClick={() => {window.location.assign(`/schools/${val.id}`);
+    return <div key={val.id} onClick={() => {history.push(`/schools/${val.id}`);
     }} className="inst">{val.name}</div>
    })}
    </div>
